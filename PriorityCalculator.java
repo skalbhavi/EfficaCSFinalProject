c66d24b593d2850dda2028a22d99983db14b2a2f
mnishi283 <mnishi283@student.fuhsd.org>
2026-04-29 12:35:36 -0700

    Basic Methods
    4/29 12:30pm

--- PriorityCalculator.java
+++ PriorityCalculator.java
@@ -1,3 +1,18 @@
 public class PriorityCalculator {
+    public static int calculatePriorityScore(Task task, String currentDate) {
+        /*
+        closer due date = higher score
+        higher custom priority = higher score
+        longer estimated time = higher score
+        completed task = very low score
+         */
+    }
 
+    public static int daysUntilDue(Task task, String currentDate) {
+
+    }
+
+    public static boolean isOverdue(Task task, String currentDate) {
+
+    }
 }
