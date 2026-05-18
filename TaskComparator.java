d2faecd525221747f3fdf69d0f57a96262ed1e97
mnishi283 <mnishi283@student.fuhsd.org>
2026-05-18 13:10:20 -0700

    Create TaskComparator.java

---
+++ TaskComparator.java
@@ -0,0 +1,14 @@
+public class TaskComparator {
+
+    public static Comparator<Task> getComparator(SortMode mode) {
+        // TODO
+    }
+
+    public static int compareByDueDate(Task a, Task b) {
+        // TODO
+    }
+
+    public static int compareByEstimatedTime(Task a, Task b) {
+        // TODO
+    }
+}
