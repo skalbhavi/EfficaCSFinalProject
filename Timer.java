0b49639494d20b514bc1319edd22c9e903a8f4c1
skalbhavi <shripriya.kalbhavi@gmail.com>
2026-05-27 12:18:53 -0700

    added timer class and fixed controller to reflect changes

--- Timer.java
+++ Timer.java
@@ -1,3 +1,73 @@
 public class Timer {
-    //implement if time allows
+    private int workMinutes;
+    private int breakMinutes;
+    private int remainingSeconds;
+    private boolean running;
+    private boolean onBreak;
+    
+    public Timer(int workMinutes, int breakMinutes) {
+        this.workMinutes = workMinutes;
+        this.breakMinutes = breakMinutes;
+        remainingSeconds = workMinutes*60;
+        running = false;
+        onBreak = false;
+    }
+
+    public void start() {
+        running = true;
+    }
+
+    public void pause() {
+        running = false;
+    }
+
+    public void reset() {
+        running = false;
+        onBreak = false;
+        remainingSeconds = workMinutes*60;
+    }
+
+    public void tick() {
+        if (running && remainingSeconds > 0) {
+            remainingSeconds--;
+        }
+        if (remainingSeconds == 0) {
+            switchMode();
+        }
+    }
+
+    public void switchMode() {
+        onBreak = !onBreak;
+        if (onBreak) {
+            remainingSeconds = breakMinutes * 60;
+        }
+        else {
+            remainingSeconds = workMinutes * 60;
+        }
+    }
+
+    public int getRemainingSeconds() {
+        return remainingSeconds;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public boolean isOnBreak() {
+        return onBreak;
+    }
+
+    public String getFormattedTime() {
+        int minutes = remainingSeconds / 60;
+        int seconds = remainingSeconds % 60;
+        String secondString;
+        if (seconds < 10) {
+            secondString = "0" + seconds;
+        }
+        else {
+            secondString = "" + seconds;
+        }
+        return minutes + ":" + secondString;
+    }
 }
