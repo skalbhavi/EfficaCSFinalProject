afba6276cf3d38015578db8df14bae86bcdf882a
aarthi-1205 <achandar251@student.fuhsd.org>
2026-05-29 19:24:46 -0700

    more comments !

--- Timer.java
+++ Timer.java
@@ -1,10 +1,24 @@
+
+/**
+ * Represents a productivity timer that alternates between
+ * work sessions & break sessions. 
+ * The timer can be started, paused, reset, & switched between
+ * work & break modes. 
+ */
+
 public class Timer {
+    
     private int workMinutes;
     private int breakMinutes;
     private int remainingSeconds;
     private boolean running;
     private boolean onBreak;
     
+    /**
+     * Creates a new timer with specified work & break durations
+     * @param workMinutes
+     * @param breakMinutes
+     */
     public Timer(int workMinutes, int breakMinutes) {
         this.workMinutes = workMinutes;
         this.breakMinutes = breakMinutes;
@@ -13,19 +27,38 @@ public class Timer {
         onBreak = false;
     }
 
+    /** 
+     * Starts the timer
+     */
+
     public void start() {
         running = true;
     }
 
+    /**
+     * Pauses the timer
+     */
+
     public void pause() {
         running = false;
     }
+
+    /**
+     * Resets the timer to the beginning of the work session
+     */
 
     public void reset() {
         running = false;
         onBreak = false;
         remainingSeconds = workMinutes*60;
     }
+
+    /**
+     * Decreases the remaining time by 1 second if 
+     * the timer is running. 
+     * When the timer reaches zero, it automatically switches
+     * between work and break modes. 
+     */
 
     public void tick() {
         if (running && remainingSeconds > 0) {
@@ -36,6 +69,10 @@ public class Timer {
         }
     }
 
+    /**
+     * Switches the timer between work & break mode 
+     * The remaining time is updated to match the selected mode
+     */
     public void switchMode() {
         onBreak = !onBreak;
         if (onBreak) {
@@ -46,21 +83,43 @@ public class Timer {
         }
     }
 
+    /**
+     * Returns the number of seconds remaining in the current session 
+     * @return the remaining time in seconds
+     */
     public int getRemainingSeconds() {
         return remainingSeconds;
     }
 
+    /**
+     * Indicates whether the timer is currently running
+     * @return true if the time running; false otherwise
+     */
     public boolean isRunning() {
         return running;
     }
 
+    /**
+     * Indicates whether the timer is currently in break mode
+     * @return true if the time is on a break; false otherwise
+     */
     public boolean isOnBreak() {
         return onBreak;
     }
 
+    /**
+     * Returns a label describing the current timer mode
+     * @return "Break Time!" if on a break; otherwise "Focusing . . ."
+     */
+
     public String getStatusLabel() {
         return onBreak ? "Break Time!" : "Focusing...";
     }
+
+    /**
+     * Returns the remaining time in MM : SS format 
+     * @return the formatted remaining time 
+     */
 
     public String getFormattedTime() {
         int minutes = remainingSeconds / 60;
